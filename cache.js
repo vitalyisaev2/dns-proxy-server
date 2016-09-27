83cdc21c881093da0f5a639944b487933c5b45d6
Elvis de Freitas <edigitalb@gmail.com>
2016-09-27 00:20:43 -0300

    revent duplicates in cache answer and adding logs

--- cache.js
+++ cache.js
@@ -5,23 +5,37 @@ module.exports = function(cache){
 	this.get = function(question){
 		var msg = cache[question.name];
 		if(msg && msg.question.filter( q => q.type == question.type ).length > 0){
+			console.log("m=get, status=found, questionName=%s, questionType=%s",
+						question.name, question.type)
 			return msg;
 		}
+		console.log("m=get, status=not-found, questionName=%s, questionType=%s",
+							question.name, question.type)
 		return null;
 	}
 
 	/**
 	 * Value must be a question
 	 */
-	this.put = function(key, value){
-	var tmp = cache[key];
+	this.put = function(question, value){
+
+		var tmp = cache[question.name];
 		if(tmp == null){
-			cache[key] = value;
+			console.log("m=put, status=puttingNewQuestion, questionName=%s, questionType=%s",
+					question.name, question.type)
+			cache[question.name] = value;
 		}else{
-			tmp.additional = tmp.additional.concat(value.additional);
-			tmp.answer= tmp.answer.concat(value.answer);
-			tmp.authority= tmp.authority.concat(value.authority);
-			tmp.question= tmp.question.concat(value.question);
+			if(!this.get(question)){
+				console.log("m=put, status=puttingNewType, questionName=%s, questionType=%s",
+					question.name, question.type)
+				tmp.additional = tmp.additional.concat(value.additional);
+				tmp.answer= tmp.answer.concat(value.answer);
+				tmp.authority= tmp.authority.concat(value.authority);
+				tmp.question= tmp.question.concat(value.question);
+			}else{
+				console.log("m=put, status=already put, questionName=%s, questionType=%s",
+				 question.name, question.type)
+			}
 		}
 	}
 
