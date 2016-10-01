d93e54afcda3d96c0233e112cc23108d80d82bbf
Elvis de Freitas <edigitalb@gmail.com>
2016-10-01 13:27:30 -0300

    cache api v2

--- hostnameController.js
+++ hostnameController.js
@@ -11,6 +11,21 @@ module.exports = function(app){
 	});
 
 	app.get('/cache', (req, res) => {
+		v2CacheAPI(req, res);
+	});
+	app.get('/v1/cache', (req, res) => {
 			res.send(app.data.cache);
 	});
+	app.get('/v2/cache', (req, res) => {
+		v2CacheAPI(req, res);
+	});
+
+	function v2CacheAPI(req, res){
+		var keys = Object.keys(app.data.cache);
+		res.send({
+			keys: keys,
+			size: keys.length,
+			data: app.data.cache
+		});
+	}
 }
